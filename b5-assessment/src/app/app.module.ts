b5fc620e6147457334e8ce256bb19c381afcb757
aaeshaker <aaeshaker@outlook.com>
2022-07-31 19:47:43 +0200

    setting ngrx environment

--- b5-assessment/src/app/app.module.ts
+++ b5-assessment/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { reducers, metaReducers } from './store';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     SharedModule,
     CoreModule,
-    HttpClientModule
+    HttpClientModule,
+    // StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
