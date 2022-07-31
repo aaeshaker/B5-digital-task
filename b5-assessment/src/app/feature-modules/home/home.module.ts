56c8e689a7210bab1021593c29292edcfaca4837
aaeshaker <aaeshaker@outlook.com>
2022-07-31 15:34:59 +0200

    dividing home

--- b5-assessment/src/app/feature-modules/home/home.module.ts
+++ b5-assessment/src/app/feature-modules/home/home.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ProductsListComponent,
+    SubHeaderComponent,
+    CategoriesComponent
   ],
   imports: [
     CommonModule,
